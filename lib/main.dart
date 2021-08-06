2d9d79d54e8720fe02d7b94d9278cce9d43a3c48
ssar@nate.com <ssar@nate.com>
2021-08-07 00:04:28 +0900

    url 추가

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
   runApp(MyApp());
 }
 
+// https://medium.com/flutter-community/server-sent-events-sse-with-flutter-cf331f978b4f
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
